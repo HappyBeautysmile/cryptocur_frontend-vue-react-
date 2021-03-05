378605a41d6fe2dcacb0ba6351d82d6e81be77f3
DarkBear0121 <darkbear0112@gmail.com>
2021-03-05 12:46:14 +0900

    fix some bugs

--- src/config/index.js
+++ src/config/index.js
@@ -3,17 +3,17 @@
 // export const ImgUrl = "https://zedcoin.org/upload/avatar/";
 
 const prod = {
-    BaseUrl : "https://zedcoin.org/api/",
-    BgUrl : "https://zedcoin.org/upload/bg/",
-    ImgUrl : "https://zedcoin.org/upload/avatar/",
-    CoinImg: "https://zedcoin.org/upload/coin_img/"
+    BaseUrl : "https://cms.zedcoin.org/api/",
+    BgUrl : "https://cms.zedcoin.org/upload/bg/",
+    ImgUrl : "https://cms.zedcoin.org/upload/avatar/",
+    CoinImg: "https://cms.zedcoin.org/upload/coin_img/"
 }
 
 const local = {
-    BaseUrl : "http://localhost:3000/api/",
-    BgUrl : "http://localhost:3000/upload/bg/",
-    ImgUrl: "http://localhost:3000/upload/avatar/",
-    CoinImg: "http://localhost:3000/upload/coin_img/"
+    BaseUrl : "http://localhost:3030/api/",
+    BgUrl : "http://localhost:3030/upload/bg/",
+    ImgUrl: "http://localhost:3030/upload/avatar/",
+    CoinImg: "http://localhost:3030/upload/coin_img/"
 }
 
 export const config = local;
